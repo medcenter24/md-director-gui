863946e6fddd49c0a163924e92d4d463997301df
MedCenter24 Dev <dev@medcenter24.com>
2017-05-08 18:55:43 +0300

    added selectors for doctors and for hospitals

---
+++ src/app/components/doctors/select/select.component.ts
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2017
+ *
+ *  @author Alexander Zagovorichev <zagovorichev@gmail.com>
+ */
+
+import { Component, Input } from '@angular/core';
+import { SlimLoadingBarService } from 'ng2-slim-loading-bar';
+import { Logger } from 'angular2-logger/core';
+import { Doctor } from '../doctor';
+import { DoctorsService } from '../doctors.service';
+@Component({
+  selector: 'select-doctor',
+  templateUrl: './select.html'
+})
+export class DoctorSelectComponent {
+
+  @Input() doctorId: number = 0;
+
+  doctors: Array<Doctor> = [];
+  filteredDoctors: Array<Doctor> = [];
+
+  constructor (private doctorsService: DoctorsService, private loadingBar: SlimLoadingBarService, private _logger: Logger) {}
+
+  ngOnInit () {
+    this.loadingBar.start();
+    this.doctorsService.getDoctors().then(doctors => {
+      this.doctors = doctors;
+      this.loadingBar.complete();
+    }).catch((err) => {
+      this.loadingBar.complete();
+      this._logger.error(err);
+    });
+  }
+
+  filterDoctors (event): void {
+    this.filteredDoctors = [];
+    for(let i = 0; i < this.doctors.length; i++) {
+      let doctor = this.doctors[i];
+      if(doctor.name.toLowerCase().indexOf(event.query.toLowerCase()) == 0) {
+        this.filteredDoctors.push(doctor);
+      }
+    }
+  }
+
+  handleDropdownClick() {
+    this.filteredDoctors = [];
+
+    //mimic remote call
+    setTimeout(() => {
+      this.filteredDoctors = this.doctors;
+    }, 100)
+  }
+}
