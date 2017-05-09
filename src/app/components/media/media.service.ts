a0630edcbe7093128601e7b27077635b3f83580b
MedCenter24 Dev <dev@medcenter24.com>
2017-05-09 17:44:37 +0300

    Added referral number

--- src/app/components/media/media.service.ts
+++ src/app/components/media/media.service.ts
@@ -1,24 +1,24 @@
 /*
- * Copyright (c) 2017.
+ * Copyright (c) 2017
  *
- * @author Alexander Zagovorichev <zagovorichev@gmail.com>
+ *  @author Alexander Zagovorichev <zagovorichev@gmail.com>
  */
 
 import {Injectable} from '@angular/core';
 import {Headers, Http} from "@angular/http";
+import { Media } from './media';
 
 @Injectable()
-export class UploadService {
+export class MediaService {
 
   private headers = new Headers({'Content-Type': 'application/json'});
-  private uploadUrl = 'director/upload';  // URL to web api
+  private mediaUrl = 'director/media';  // URL to web api
 
   constructor(private http: Http) { }
 
   getUploadUrl(): string {
-    return this.uploadUrl;
+    return this.mediaUrl;
   }
-
 
   private handleError(error: any): Promise<any> {
     return Promise.reject(error.message || error);
--- src/app/faker/media.db.ts
+++ src/app/faker/media.db.ts
@@ -4,7 +4,7 @@
  * @author Alexander Zagovorichev <zagovorichev@gmail.com>
  */
 
-import {Media} from "../components/media/media";
+import { Media } from '../components/media/media';
 export const MediaDb: Media[] = [
-    {id: 1, path: 'assets/img/app/profile/Vlad.png', alt: 'Profile picture Vlad'},
+  {id: 1, path: 'assets/img/app/profile/Vlad.png', alt: 'Profile picture Vlad'},
 ].map(x => new Media(x.id, x.path, x.alt));
