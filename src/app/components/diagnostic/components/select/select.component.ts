496be969cc390c8893c4ff81eaff03aa25258d08
MedCenter24 Dev <dev@medcenter24.com>
2017-05-12 17:02:46 +0300

    added diagnostics

---
+++ src/app/components/diagnostic/components/select/select.component.ts
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2017
+ *
+ *  @author Alexander Zagovorichev <zagovorichev@gmail.com>
+ */
+
+import { Component, Output, EventEmitter, Input } from '@angular/core';
+import { DiagnosticService } from '../../diagnostic.service';
+import { SelectItem } from 'primeng/primeng';
+import { Diagnostic } from '../../diagnostic';
+import { SlimLoadingBarService } from 'ng2-slim-loading-bar';
+import { Logger } from 'angular2-logger/core';
+
+@Component({
+  selector: 'select-diagnostics',
+  templateUrl: './select.html'
+})
+export class SelectDiagnosticsComponent {
+
+  @Output() loaded: EventEmitter<string> = new EventEmitter<string>();
+  @Output() chosenDiagnosticsChange: EventEmitter<Diagnostic[]> = new EventEmitter<Diagnostic[]>();
+
+  @Input() chosenDiagnostics: Array<Diagnostic> = [];
+
+  dataDiagnostics: SelectItem[] = [];
+  selectedDiagnostics: Array<string> = [];
+  diagnostics: Array<Diagnostic> = [];
+
+  constructor (
+    private diagnosticsService: DiagnosticService,
+    private loadingBar: SlimLoadingBarService,
+    private _logger: Logger
+  ) {
+  }
+
+  ngOnInit () {
+    this.loadingBar.start();
+    this.diagnosticsService.getDiagnostics().then(diagnostics => {
+      this.diagnostics = diagnostics;
+      this.dataDiagnostics = diagnostics.map(x => {
+        return {
+          label: '' + x.title,
+          value: '' + x.id
+        };
+      });
+
+      if (!this.selectedDiagnostics.length) {
+        // to show placeholder
+        this.selectedDiagnostics = [];
+      }
+      this.loadingBar.complete();
+      this.loaded.emit();
+    }).catch((err) => {
+      this.loadingBar.complete();
+      this._logger.error(err);
+    });
+  }
+
+   onChanged(event): void {
+     const diagnostics = this.diagnostics.filter(function (diagnostic) {
+       return event.value.indexOf(diagnostic.id+'') !== -1;
+     });
+
+     this.chosenDiagnosticsChange.emit(diagnostics);
+   }
+
+   reloadChosenDiagnostics(diagnostics: Array<Diagnostic>): void {
+     this.chosenDiagnostics = diagnostics;
+     if (this.chosenDiagnostics.length) {
+       this.selectedDiagnostics = this.chosenDiagnostics.map(x => x.id + '');
+     }
+   }
+}
