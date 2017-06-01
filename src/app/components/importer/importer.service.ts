86112e966a7e5ee1e11dfd35795a3cb01fc767c1
MedCenter24 Dev <dev@medcenter24.com>
2017-06-01 19:00:57 +0300

    Importer

--- src/app/components/importer/importer.service.ts
+++ src/app/components/importer/importer.service.ts
@@ -32,11 +32,11 @@ export class ImporterService {
       .catch(this.handleError);
   }
 
-  importFile(url:string, id: number) : Promise<void> {
+  importFile(url:string, id: number) : Promise<any> {
     url = `${url}/${id}`;
     return this.http.put(url, [], {headers: this.headers})
       .toPromise()
-      .then(() => null)
+      .then((response) => response.json())
       .catch(this.handleError);
   }
 
