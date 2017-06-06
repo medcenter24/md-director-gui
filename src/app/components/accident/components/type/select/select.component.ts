22e9ee36791c67b9d213d261acfa0637ec0afdc4
MedCenter24 Dev <dev@medcenter24.com>
2017-06-06 18:55:09 +0300

    fixed Score counter, etc

--- src/app/components/accident/components/type/select/select.component.ts
+++ src/app/components/accident/components/type/select/select.component.ts
@@ -9,6 +9,7 @@ import { AccidentType } from '../type';
 import { AccidentTypesService } from '../types.service';
 import { SlimLoadingBarService } from 'ng2-slim-loading-bar';
 import { Logger } from 'angular2-logger/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'select-accident-type',
@@ -23,14 +24,26 @@ export class SelectAccidentTypeComponent {
   private dataItems: Array<any> = [];
   private selectedType: AccidentType = new AccidentType();
   private loadedTypes: AccidentType[] = [];
+  private transInsurance: string = '';
+  private transNonInsurance: string = '';
 
   constructor (
     private accidentTypesService: AccidentTypesService,
     private loadingBar: SlimLoadingBarService,
+    private translate: TranslateService,
     private _logger: Logger
   ) { }
 
   ngOnInit () {
+
+    this.translate.get('general.insurance').subscribe(res => {
+      this.transInsurance = res;
+    });
+
+    this.translate.get('general.non-insurance').subscribe(res => {
+      this.transNonInsurance = res;
+    });
+
     this.loadingBar.start();
     this.accidentTypesService.getTypes().then(types => {
       this.loadedTypes = types;
@@ -40,7 +53,7 @@ export class SelectAccidentTypeComponent {
       }
       this.dataItems = types.map(x => {
         return {
-          label: x.title,
+          label: x.title === 'insurance' ? this.transInsurance : this.transNonInsurance,
           value: x.id
         };
       });
