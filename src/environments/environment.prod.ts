f56aaf4f9ad8834b60e0d52059067ad1ff0f4950
MedCenter24 Dev <dev@medcenter24.com>
2017-09-22 22:33:25 -0500

    fixed prod url to the api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,7 +7,7 @@
 import { Level } from 'angular2-logger/core';
 export const environment = {
   production: true,
-  apiHost: 'http://medical.company.dhv24.com',
+  apiHost: 'http://medical.company.dhv24.com/api',
   logger: {
     level: Level.WARN,
   },
