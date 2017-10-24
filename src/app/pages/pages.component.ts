db791d977f8a982fc235dd2d3278a062f53de7b8
MedCenter24 Dev <dev@medcenter24.com>
2017-10-24 19:01:09 +0300

    new styles

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -7,33 +7,33 @@ import { PAGES_MENU } from './pages.menu';
 @Component({
   selector: 'pages',
   template: `
-    <ba-sidebar></ba-sidebar>
-    <ba-page-top></ba-page-top>
-    <div class="al-main">
-      <div class="al-content">
-        <ba-content-top></ba-content-top>
-        <router-outlet></router-outlet>
-      </div>
-    </div>
-    <footer class="al-footer clearfix">
-      <div class="al-footer-right">
-          <div class="copyright">
-              &copy; 2017 MyDoctors24
+      <ba-sidebar></ba-sidebar>
+      <ba-page-top></ba-page-top>
+      <div class="al-main">
+          <div class="al-content">
+              <ba-content-top></ba-content-top>
+              <router-outlet></router-outlet>
           </div>
-          <img src="/assets/img/medical/medical_120.png" width="120"
-               height="120" alt="{{ 'Medical Company' | translate }}">
       </div>
-      <div class="al-footer-main clearfix">
-        <ul class="al-share clearfix">
-          <li><i class="socicon socicon-facebook"></i></li>
-          <li><i class="socicon socicon-twitter"></i></li>
-          <li><i class="socicon socicon-google"></i></li>
-          <li><i class="socicon socicon-youtube"></i></li>
-        </ul>
-      </div>
-    </footer>
-    <ba-back-top position="200"></ba-back-top>
-    `,
+      <footer class="al-footer clearfix">
+          <div class="al-footer-right">
+              <div class="copyright">
+                  &copy; 2017 MyDoctors24
+              </div>
+              <img src="/assets/img/medical/medical_120.png" width="120"
+                   height="120" alt="{{ 'Medical Company' | translate }}">
+          </div>
+          <div class="al-footer-main clearfix">
+              <ul class="al-share clearfix">
+                  <li><i class="socicon socicon-facebook"></i></li>
+                  <li><i class="socicon socicon-twitter"></i></li>
+                  <li><i class="socicon socicon-google"></i></li>
+                  <li><i class="fa fa-youtube-play" style="background-color: #ff3a38;"></i></li>
+              </ul>
+          </div>
+      </footer>
+      <ba-back-top position="200"></ba-back-top>
+  `,
 })
 export class Pages implements OnInit {
 
