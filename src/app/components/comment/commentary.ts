908b5c66f055678f703138663db4b8ee0081c3d8
Alexander Zagovorychev <zagovorichev@gmail.com>
2018-01-25 19:46:26 +0300

    Comments added to the Case

---
+++ src/app/components/comment/commentary.ts
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2018.
+ *
+ * @author Zagovorychev Olexandr <zagovorichev@gmail.com>
+ */
+
+export class Commentary {
+  constructor(
+    public id: number = 0,
+    public user_id: number = 0,
+    public user_name: string = '',
+    public user_thumb: string = '',
+    public body: string = '',
+    public created_at: string = '',
+  ) {}
+}
