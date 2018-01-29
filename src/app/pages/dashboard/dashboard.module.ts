b9d688e062864d946ce9a8fdf97ac00bf0922af7
MedCenter24 Dev <dev@medcenter24.com>
2018-01-29 14:16:49 +0300

    Dashboard statistics

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -1,27 +1,24 @@
-import { NgModule }      from '@angular/core';
-import { CommonModule }  from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NgaModule } from '../../theme/nga.module';
 
-import { Dashboard } from './dashboard.component';
-import { routing }       from './dashboard.routing';
+import { DashboardComponent } from './dashboard.component';
+import { routing } from './dashboard.routing';
 
 import { PopularApp } from './popularApp';
 import { PieChart } from './pieChart';
 import { TrafficChart } from './trafficChart';
-import { UsersMap } from './usersMap';
-import { LineChart } from './lineChart';
 import { Feed } from './feed';
 import { Todo } from './todo';
-import { Calendar } from './calendar';
+import { CalendarComponent } from './calendar';
 import { CalendarService } from './calendar/calendar.service';
 import { FeedService } from './feed/feed.service';
-import { LineChartService } from './lineChart/lineChart.service';
 import { PieChartService } from './pieChart/pieChart.service';
 import { TodoService } from './todo/todo.service';
 import { TrafficChartService } from './trafficChart/trafficChart.service';
-import { UsersMapService } from './usersMap/usersMap.service';
 import { AppTranslationModule } from '../../app.translation.module';
+import { CalendarModule } from 'angular-calendar';
 
 @NgModule({
   imports: [
@@ -30,26 +27,23 @@ import { AppTranslationModule } from '../../app.translation.module';
     NgaModule,
     routing,
     AppTranslationModule,
+    CalendarModule.forRoot(),
   ],
   declarations: [
     PopularApp,
     PieChart,
     TrafficChart,
-    UsersMap,
-    LineChart,
     Feed,
     Todo,
-    Calendar,
-    Dashboard
+    CalendarComponent,
+    DashboardComponent,
   ],
   providers: [
     CalendarService,
     FeedService,
-    LineChartService,
     PieChartService,
     TodoService,
     TrafficChartService,
-    UsersMapService
-  ]
+  ],
 })
 export class DashboardModule {}
