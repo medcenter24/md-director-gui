1b21f730ba8ffa98c71f95341b7d208f971dfac1
MedCenter24 Dev <dev@medcenter24.com>
2018-01-31 18:45:12 +0300

    Added timezones

--- src/app/pages/dashboard/calendar/calendar.component.ts
+++ src/app/pages/dashboard/calendar/calendar.component.ts
@@ -1,20 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CalendarService } from './calendar.service';
 import * as jQuery from 'jquery';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'nga-calendar',
   templateUrl: './calendar.html',
   styleUrls: ['./calendar.scss'],
 })
-export class CalendarComponent {
+export class CalendarComponent implements OnInit {
 
-  calendarConfiguration: any;
+  calendarConfiguration: any = false;
   private _calendar: Object;
 
-  constructor(private _calendarService: CalendarService) {
-    this.calendarConfiguration = this._calendarService.getData();
-    this.calendarConfiguration.select = (start, end) => this._onSelect(start, end);
+  constructor(private _calendarService: CalendarService, private _translateService: TranslateService) {
+  }
+
+  ngOnInit() {
+    this._translateService.get('Today').subscribe(() => {
+      this.calendarConfiguration = {
+        buttonIcons: {
+          prev: 'left-single-arrow',
+          next: 'right-single-arrow',
+          prevYear: 'left-double-arrow',
+          nextYear: 'right-double-arrow',
+        },
+        buttonText: {
+          today: this._translateService.instant('Today'),
+          month: this._translateService.instant('Month'),
+          week: this._translateService.instant('Week'),
+          day: this._translateService.instant('Day'),
+        },
+        header: {
+          left: 'prevYear,prev,next,nextYear today',
+          center: 'title',
+          right: 'month,agendaWeek,agendaDay',
+        },
+        locale: this._translateService.currentLang,
+        defaultDate: new Date(),
+        selectable: true,
+        selectHelper: true,
+        editable: false,
+        eventLimit: false,
+        events: (start, end, timezone, callback) => {
+          this._calendarService
+            .loadEvents(start, end)
+            .then(events => {
+              callback(events);
+            });
+        },
+      };
+    });
+
+    // jQuery(this._calendar).fullCalendar( { events: this._calendarService.getUrl() });
+
+    // this._calendarService.loadCalendar('').then(config => {
+      // console.log(config)
+      // this.calendarConfiguration = this._calendarService.getData();
+      // this.calendarConfiguration.select = (start, end) => this._onSelect(start, end);
+    // }).catch();
   }
 
   onCalendarReady(calendar): void {
