98dcf011412f487292632637f34816c665393899
MedCenter24 Dev <dev@medcenter24.com>
2018-05-23 17:50:54 +0300

    Loadable providers

--- src/app/components/core/loadable/provider/test/sample/pagination.data.sample.ts
+++ src/app/components/core/loadable/provider/test/sample/pagination.data.sample.ts
@@ -7,23 +7,30 @@
 import { SearchableServiceInterface } from '../../../searchable.service.interface';
 import { SearchFilter } from '../../../search.filter';
 
-export class SingleDataSample implements SearchableServiceInterface {
+export class PaginationDataSample implements SearchableServiceInterface {
 
   private numTry: number = 0;
+
+  private data: any[] = [{
+    id: 1,
+    name: 'Peter',
+    description: 'doctor',
+  }, {
+    id: 2,
+    name: 'Foster',
+    description: 'director',
+  }, {
+    id: 3,
+    name: 'Abigail',
+    description: 'user',
+  }];
 
   search(filters: SearchFilter): Promise<any> {
     return new Promise<any>((resolve, reject) => {
       this.numTry++;
       resolve({
-        data: [{
-          id: 1,
-          name: 'Peter',
-          description: 'doctor',
-        }, {
-          id: 2,
-          name: 'Foster',
-          description: 'director',
-        }],
+        // pretty simple pagination
+        data: this.data.slice(filters.first, filters.first + filters.rows),
       });
     });
   }
--- src/app/components/core/loadable/provider/test/single.loadable.provider.spec.ts
+++ src/app/components/core/loadable/provider/test/single.loadable.provider.spec.ts
@@ -6,6 +6,7 @@
 
 import { SingleLoadableProvider } from '../single.loadable.provider';
 import { SingleDataSample } from './sample/single.data.sample';
+import { SearchFilter } from '../../search.filter';
 
 describe('Loadable: Single Provider', () => {
   let service: SingleLoadableProvider;
@@ -18,10 +19,11 @@ describe('Loadable: Single Provider', () => {
 
   it('checkSearch', () => {
     expect(conf.getNumTry()).toBe(0);
-    service.search({
-      field: 'id',
+    const filter: SearchFilter = SearchFilter.instance({
+      fields: ['id'],
       query: '123',
-    })
+    });
+    service.search(filter)
       .then(data => expect(data).toEqual([]) )
       .catch(() => expect().nothing());
     expect(conf.getNumTry()).toBe(1);
@@ -29,22 +31,55 @@ describe('Loadable: Single Provider', () => {
 
   it('checkException', () => {
     expect(conf.getNumTry()).toBe(0);
-    service.search({})
-      .then(data => expect(data).toEqual([{}]))
+    const filter = SearchFilter.instance();
+    service.search(filter)
+      .then(data => expect(data).toEqual([]))
       .catch(msg => expect(msg).toBe('Single Loadable Provider must have [field] and [query] filters parameters'));
     expect(conf.getNumTry()).toBe(1);
   });
 
-  it ('checkFiltering', () => {
-    service.search({
-      field: 'id',
+  it ('checkFiltering', done => {
+    const filter = SearchFilter.instance({
+      fields: ['id'],
       query: 1,
-    })
-      .then(data => expect(data).toEqual([{
-        id: 1,
-        name: 'Peter',
-        description: 'doctor',
-      }]))
-      .catch(msg => expect(msg).toBeNull());
+    });
+    service.search(filter)
+      .then(data => {
+        expect(data).toEqual([{
+          id: 1,
+          name: 'Peter',
+          description: 'doctor',
+        }]);
+        done();
+      })
+      .catch(msg => {
+        expect(msg).toBeNull();
+        done();
+      });
+  });
+
+  it ('single provider returns all filtered data without pagination', done => {
+    const filter = SearchFilter.instance({
+      fields: ['id'],
+      query: '',
+      rows: 1,
+    });
+    service.search(filter)
+      .then(data => {
+        expect(data).toEqual([{
+          id: 1,
+          name: 'Peter',
+          description: 'doctor',
+        }, {
+          id: 2,
+          name: 'Foster',
+          description: 'director',
+        }]);
+        done();
+      })
+      .catch(msg => {
+        expect(msg).toBeNull();
+        done();
+      });
   });
 });
