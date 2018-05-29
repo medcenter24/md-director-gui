08599c24b474d30ceaf227e2efbd93a632e10a51
MedCenter24 Dev <dev@medcenter24.com>
2018-05-29 16:44:21 +0300

    dashboard improvement

--- src/app/pages/dashboard/calendar/calendar.service.ts
+++ src/app/pages/dashboard/calendar/calendar.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpService } from '../../../components/core/http/http.service';
 import { CalendarEvent } from './calendar';
+import { Moment } from 'moment';
 
 @Injectable()
 export class CalendarService extends HttpService {
@@ -12,8 +13,8 @@ export class CalendarService extends HttpService {
   /**
    * @returns {Promise<any>}
    */
-  loadEvents(start, end): Promise<CalendarEvent[]> {
-    return this.get(null, `start=${start.format('YYYY-MM-DD')}&end=${end.format('YYYY-MM-DD')}`)
+  loadEvents(start: Moment, end: Moment): Promise<CalendarEvent[]> {
+    return this.get(null, { start: start.format('YYYY-MM-DD'), end: end.format('YYYY-MM-DD') })
       .then(response => response.data as CalendarEvent[]);
   }
 }
