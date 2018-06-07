6b9a3991da15fa0c71a41789f5a76369282fe19f
Alexander Zagovorychev <zagovorichev@gmail.com>
2018-06-08 00:01:30 +0300

    Autocompleter refactored

---
+++ src/app/components/ui/selector/components/autocompleter/index.ts
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2018.
+ *
+ * @author Zagovorychev Oleksandr <zagovorichev@gmail.com>
+ */
+
+export * from './autocompleter.component';
+export * from './autocompleter.module';
--- src/app/pages/development/development.page.module.ts
+++ src/app/pages/development/development.page.module.ts
@@ -10,6 +10,7 @@ import { routing } from './development.page.routing';
 import { DevelopmentGuiModule } from './gui';
 import { DevelopmentGuiMultiselectModule } from './gui/multiselect';
 import { MultiselectImplementationsModule } from './gui/multiselect/implementations';
+import { DevelopmentGuiAutocompleterModule } from './gui/autocompleter';
 
 @NgModule({
   imports: [
@@ -17,6 +18,7 @@ import { MultiselectImplementationsModule } from './gui/multiselect/implementati
     DevelopmentGuiModule,
     DevelopmentGuiMultiselectModule,
     MultiselectImplementationsModule,
+    DevelopmentGuiAutocompleterModule,
   ],
   declarations: [
     DevelopmentPageComponent,
