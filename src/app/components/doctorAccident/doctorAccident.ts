c912bbf289419e9f6fc80a53d778d84f09296e2c
Oleksander Zagovorychev <zagovorichev@gmail.com>
2018-11-30 15:30:55 +0300

    - scenario styles - case editor fields

--- src/app/components/doctorAccident/doctorAccident.ts
+++ src/app/components/doctorAccident/doctorAccident.ts
@@ -8,16 +8,10 @@ export class DoctorAccident {
 
   constructor (
       public id: number = 0,
-      public accidentId: number = 0,
-      public accidentStatusId: number = 0,
       public doctorId: number = 0,
-      public cityId: number = 0,
-      public status: string = '',
       public recommendation: string = '',
       public investigation: string = '',
       public visitTime: string = '',
       public createdAt: string = '',
-      public updatedAt: string = '',
-      public deletedAt: string = '',
   ) { }
 }
