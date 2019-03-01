64438d477bb9966decf1825f3574e047da821ce1
zagovorychev <zagovorichev@gmail.com>
2019-03-01 10:13:01 +0300

    fixed prod address

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,7 +8,7 @@ import { Level } from 'angular2-logger/core';
 export const environment = {
   production: true,
   // prod
-  apiHost: 'http://api.medical.company.dhv24.com',
+  apiHost: 'http://api.mydocs24.com',
   // test preprod nginx
   // apiHost: 'http://mydoctors24.api.loc',
   logger: {
